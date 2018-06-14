05119a32f6e37a1545dfc2fd04ad9548d9ff0357
Malcolm Jones <malcolm@adobe.com>
2018-06-14 12:34:39 -0400

    chg: more stuff we might want

--- brew-deps.sh
+++ brew-deps.sh
@@ -30,3 +30,29 @@ export LDFLAGS="-L$(brew --prefix gettext)/lib $LDFLAGS"
 export CPPFLAGS="-I$(brew --prefix gettext)/include $CPPFLAGS"
 
 # make sure it's python 2.7 before running the next step
+
+# MORE: https://github.com/facebook/hhvm/wiki/Building-and-installing-HHVM-on-OSX-10.8-With-Homebrew
+# CMAKE_INCLUDE_PATH="/usr/local/include:/usr/include" \
+# CMAKE_LIBRARY_PATH="/usr/local/lib:/usr/lib" \
+# ICU_INCLUDE_DIR=$(brew --prefix icu4c)/include \
+# ICU_LIBRARY=$(brew --prefix icu4c)/lib/libicuuc.dylib \
+# ICU_I18N_LIBRARY=$(brew --prefix icu4c)/lib/libicui18n.dylib \
+# ICU_DATA_LIBRARY=$(brew --prefix icu4c)/lib/libicudata.dylib \
+# READLINE_INCLUDE_DIR=$(brew --prefix readline)/include \
+# READLINE_LIBRARY=$(brew --prefix readline)/lib/libreadline.dylib \
+# NCURSES_LIBRARY=$(brew --prefix ncurses)/lib/libncurses.dylib \
+# CURL_INCLUDE_DIR=$(brew --prefix curl)/include \
+# CURL_LIBRARY=$(brew --prefix curl)/lib/libcurl.dylib \
+# BOOST_INCLUDEDIR=$(brew --prefix boost)/include \
+# BOOST_LIBRARYDIR=$(brew --prefix boost)/lib \
+# Boost_USE_STATIC_LIBS=ON \
+# JEMALLOC_INCLUDE_DIR=$(brew --prefix jemalloc)/include \
+# JEMALLOC_LIB=$(brew --prefix jemalloc)/lib/libjemalloc.dylib \
+# LIBINTL_LIBRARIES=$(brew --prefix gettext)/lib/libintl.dylib \
+# LIBINTL_INCLUDE_DIR=$(brew --prefix gettext)/include \
+# LIBDWARF_LIBRARIES=$(brew --prefix libdwarf)/lib/libdwarf.3.dylib \
+# LIBDWARF_INCLUDE_DIRS=$(brew --prefix libdwarf)/include \
+# LIBMAGICKWAND_INCLUDE_DIRS=$(brew --prefix imagemagick)/include/ImageMagick-6 \
+# LIBMAGICKWAND_LIBRARIES=$(brew --prefix imagemagick)/lib/libMagickWand-6.Q16.dylib \
+# MYSQL_INCLUDE_DIR=$(brew --prefix mysql-connector-c)/include \
+# MYSQL_LIB=$(brew --prefix mysql-connector-c)/lib
